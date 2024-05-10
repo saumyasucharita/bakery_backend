5593a56911e09bbee30b8f9adf17ab0512599389
Saumya Sucharita <saumyasucharita@Saumyas-Laptop.local>
2024-05-09 21:19:42 -0700

    Configured Postgres DB in settings.py

--- bakery_backend/settings.py
+++ bakery_backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+     'bakeryapp',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'bakery_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bakerydb',
+        'USER': 'bakeryadmin',
+        'PASSWORD': 'teamwork',
+        'HOST': 'localhost'
     }
 }
 
